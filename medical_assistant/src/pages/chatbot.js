4f974838f74666c6f2c014f2eab21915d6d7ca42
hizem <ghassen.hizem@supcom.tn>
2023-02-25 12:00:34 +0100

    init

--- medical_assistant/src/pages/chatbot.js
+++ medical_assistant/src/pages/chatbot.js
@@ -2,7 +2,12 @@ export default function ChatBot() {
 
     return(
         <>
-        <p>testing</p>
+        <div className="w-full h-screen custom-background">
+            test
+            <div className="ml-24 mr-24 mt-12 h-5/6 shadow-[rgba(0,_0,_0,_0.4)_0px_30px_90px] bg-white">
+                bro
+            </div>
+        </div>
         </>
     )
 
