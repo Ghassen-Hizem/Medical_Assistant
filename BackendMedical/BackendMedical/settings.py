84d5723d48a67c22ed63fdd2d25ebba6fc2bb05c
hizem <ghassen.hizem@supcom.tn>
2023-04-09 22:31:20 +0100

    correction

--- BackendMedical/BackendMedical/settings.py
+++ BackendMedical/BackendMedical/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+from dotenv import dotenv_values
+config = dotenv_values(".env")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--nbafm1uk1v8ill0%wkv#+o!3k7e60(tv!p#^vz3ds1vow&p!i'
+SECRET_KEY = config.SECRET_KEY
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
