3898621619789268c01e07f808801a6d236abcd9
hizem <ghassen.hizem@supcom.tn>
2023-04-10 00:21:00 +0100

    todo

--- BackendMedical/bioBertApi/views.py
+++ BackendMedical/bioBertApi/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+
+from django.http import JsonResponse
 
 def index(request):
-    return HttpResponse("init api")
+    a = {"name" : "init api"}
+    return JsonResponse(a)
 
 
