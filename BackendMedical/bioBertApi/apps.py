8ce2d6bfc457c3407aeaaa577a2043d8e5acd35c
hizem <ghassen.hizem@supcom.tn>
2023-04-16 22:36:24 +0100

    loading the model

--- BackendMedical/bioBertApi/apps.py
+++ BackendMedical/bioBertApi/apps.py
@@ -3,11 +3,9 @@ import html
 import pathlib
 import os
 
-from bioBertApi.model import Bert
 
 #TO_DO 
 class BiobertapiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'bioBertApi'
-    MODEL_PATH = pathlib.Path("model")
-    BERT_PRETRAINED_PATH = Path("model")
+    
